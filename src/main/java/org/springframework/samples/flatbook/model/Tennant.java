6a91e6481abcacc02c2a553b1b665d4e40a10bff
Daniel <dansanbal@alum.us.es>
2020-03-15 14:42:39 +0100

    #44: Creacion de usuarios

--- src/main/java/org/springframework/samples/flatbook/model/Tennant.java
+++ src/main/java/org/springframework/samples/flatbook/model/Tennant.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,18 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.samples.flatbook.repository.springdatajpa;
 
-import org.springframework.data.repository.Repository;
-import org.springframework.samples.flatbook.model.Visit;
-import org.springframework.samples.flatbook.repository.VisitRepository;
+package org.springframework.samples.flatbook.model;
 
-/**
- * Spring Data JPA specialization of the {@link VisitRepository} interface
- *
- * @author Michael Isvy
- * @since 15.1.2013
- */
-public interface SpringDataVisitRepository extends VisitRepository, Repository<Visit, Integer> {
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Tennant extends Person {
 
 }
